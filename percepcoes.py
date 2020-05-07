1a9a84985a713f6c343437746ba8880e3ee652a9
leonardodeo <leonardodeo01@gmail.com>
2020-05-07 11:39:48 -0300

    Esqueleto

---
+++ percepcoes.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class PercepcoesJogador():
+    '''Coloque aqui atributos que descrevam as percepções possíveis de
+    mundo por parte do agente jogador
+    
+    Vide documentação sobre dataclasses em python.
+    '''
+    pass
